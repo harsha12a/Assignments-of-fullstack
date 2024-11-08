f8ce30b35bcf93e51bdfdb947dd1ef6013a72d87
harha12a <harsha.pss2005@gmail.com>
2024-11-08 21:35:38 +0530

    add:3d cart effect

--- Week-9/React-Assignment-2/task-2.2/src/Userlist/User.jsx
+++ Week-9/React-Assignment-2/task-2.2/src/Userlist/User.jsx
@@ -1,12 +1,28 @@
 import "./User.css";
 function User(props) {
+  let mousemove = (e) => {
+    let ele = e.currentTarget;
+    let rect = ele.getBoundingClientRect();
+    let skX = e.clientX - rect.left;
+    let skY = e.clientY - rect.top;
+    let Y = (skX / rect.width - 0.5) * 20;
+    let X = (skY / rect.height - 0.5) * -20;
+    ele.style.transform = `rotateX(${X}deg) rotateY(${Y}deg)`;
+  };
+
+  let mouseleave = (e) => {
+    let ele = e.currentTarget;
+    ele.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  };
   return (
-    <div className="cards">
-      <img src={props.x.image} alt="" className="pic" />
-      <img src={props.x.logo} alt="" className="logos" />
-      <h3>{props.x.hea}</h3>
-      <p>{props.x.desc}</p>
-      <h4>Name - {props.x.name}</h4>
+    <div className="container">
+      <div className="cards" onMouseMove={mousemove} onMouseLeave={mouseleave}>
+        <img src={props.x.image} alt="" className="pic" />
+        <img src={props.x.logo} alt="" className="logos" />
+        <h3>{props.x.hea}</h3>
+        <p>{props.x.desc}</p>
+        <h4>Name - {props.x.name}</h4>
+      </div>
     </div>
   );
 }
